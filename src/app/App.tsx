20bbec71c01e71338fa20e800c364d118c03f924
Yaroslav_Demin <demin@lanit.ru>
2023-10-21 18:52:32 +0300

    Добавлен конфиг для роутов

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import './styles/index.scss';
-import {
-    Link,
-    Route,
-    Routes
-} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
-import {AboutPageAsync} from "pages/AboutPage";
-import {MainPageAsync} from "pages/MainPage";
+
 import {UseTheme} from "app/providers/ThemePropvider";
 import {classNames} from "shared/lib/classNames/classNames";
+import {AppRouter} from "app/providers/router";
 
 const App = () => {
     const {theme, toggleTheme} = UseTheme();
@@ -23,18 +19,7 @@ const App = () => {
             <Link to={'/about'}>
                 О сайте
             </Link>
-            <React.Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                        <Route
-                            path={'/about'}
-                            element={<AboutPageAsync />}
-                        />
-                        <Route
-                            path={'/'}
-                            element={<MainPageAsync />}
-                        />
-                </Routes>
-            </React.Suspense>
+            <AppRouter />
 
         </div>
     );
---
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+    Route,
+    Routes,
+} from "react-router-dom";
+import {routeConfig} from "shared/config/routeConfig/routeConfig";
+
+export const AppRouter = () => {
+    return (
+        <React.Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+                {Object
+                    .values(routeConfig)
+                    .map(({element, path})=>(
+                    <Route
+                        key={path}
+                        element={element}
+                        path={path}
+                    />
+                ))}
+            </Routes>
+        </React.Suspense>
+    );
+};
+
+AppRouter.displayName = 'AppRouter';
