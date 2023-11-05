1bad269fd5394d3990f9922f32979bfffb29448f
Yaroslav_Demin <demin@lanit.ru>
2023-11-06 02:29:22 +0300

    Add: ErrorBoundary

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { BugButton } from 'widgets/PageError';
 
 const MainPage = () => {
     const { t } = useTranslation('main');
 
     return (
         <div>
+            <BugButton />
             {t('Главная страница')}
         </div>
     );
---
+++ src/widgets/PageError/index.ts
@@ -0,0 +1,2 @@
+export { BugButton } from 'widgets/PageError/ui/BugButton';
+export { PageError } from 'widgets/PageError/ui/PageError';
