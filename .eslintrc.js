933aa04feee66fcf6b14ccab6b1822a1b1ba50e8
Yaroslav_Demin <demin@lanit.ru>
2023-11-19 13:44:27 +0300

    Add: storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,73 +1,64 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-        jest: true,
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'plugin:i18next/recommended', 'plugin:storybook/recommended', 'plugin:storybook/recommended', 'plugin:storybook/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    extends: [
-        'plugin:react/recommended',
-        'airbnb',
-        'plugin:i18next/recommended',
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-    },
-    plugins: [
-        'react',
-        '@typescript-eslint',
-        'i18next',
-    ],
-    rules: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint', 'i18next'],
+  rules: {
     // 2 - это warning, identMode 4 - отступ равен 4 пробелам
     // 'react/jsx-indent': [2, {indentMode: 4, ignoreTernaryOperator: true}],
     // Идентичная запись
-        'react/jsx-indent': [2, 4],
-        'react/jsx-indent-props': [2, 4],
-        indent: [2, 4],
-        'react/jsx-filename-extension': [
-            2,
-            { extensions: ['.js', '.jsx', '.tsx'] }],
-        // в проекте используются относительные пути
-        'import/no-unresolved': 'off',
-        // правило указывать расширение пути. Но вебпак уже настроен и правило не нужно
-        'import/extensions': 'off',
-        'import/prefer-default-export': 'off',
-        'no-unused-vars': 'warn',
-        // не обязательно импортировать React с 17й версии
-        'react/react-in-jsx-scope': 'off',
-        'react/require-default-props': 'off',
-        'react/jsx-props-no-spreading': 'warn',
-        'react/function-component-definition': 'off',
-        'no-shadow': 'off',
-        // проверить что это и возможно нужно вернуть
-        'import/no-extraneous-dependencies': 'off',
-        'no-underscore-dangle': 'off',
-
-        // accessibility
-        'jsx-a11y/label-has-associated-control': 'off',
-        'jsx-a11y/click-events-have-key-events': 'off',
-        'jsx-a11y/no-noninteractive-element-interactions': 'off',
-        'i18next/no-literal-string': ['error', {
-            markupOnly: true,
-            ignoreAttribute: ['data-testiid'],
-        }],
-
-        'max-len': ['error', { ignoreComments: true, code: 120 }],
-    },
-    globals: {
-        __IS_DEV__: true,
-    },
-    overrides: [
-        {
-            files: ['**/src/**/*.test.{ts,tsx}'],
-            rules: {
-                'i18next/no-literal-string': 'off',
-            },
-        },
-    ],
+    'react/jsx-indent': [2, 4],
+    'react/jsx-indent-props': [2, 4],
+    indent: [2, 4],
+    'react/jsx-filename-extension': [2, {
+      extensions: ['.js', '.jsx', '.tsx']
+    }],
+    // в проекте используются относительные пути
+    'import/no-unresolved': 'off',
+    // правило указывать расширение пути. Но вебпак уже настроен и правило не нужно
+    'import/extensions': 'off',
+    'import/prefer-default-export': 'off',
+    'no-unused-vars': 'warn',
+    // не обязательно импортировать React с 17й версии
+    'react/react-in-jsx-scope': 'off',
+    'react/require-default-props': 'off',
+    'react/jsx-props-no-spreading': 'warn',
+    'react/function-component-definition': 'off',
+    'no-shadow': 'off',
+    // проверить что это и возможно нужно вернуть
+    'import/no-extraneous-dependencies': 'off',
+    'no-underscore-dangle': 'off',
+    // accessibility
+    'jsx-a11y/label-has-associated-control': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-noninteractive-element-interactions': 'off',
+    'i18next/no-literal-string': ['error', {
+      markupOnly: true,
+      ignoreAttribute: ['data-testiid', 'to']
+    }],
+    'max-len': ['error', {
+      ignoreComments: true,
+      code: 120
+    }]
+  },
+  globals: {
+    __IS_DEV__: true
+  },
+  overrides: [{
+    files: ['**/src/**/*.test.{ts,tsx}'],
+    rules: {
+      'i18next/no-literal-string': 'off'
+    }
+  }]
 };
