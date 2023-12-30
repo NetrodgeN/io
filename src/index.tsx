ab6cbefcafd27520e3439768fe2d4f102b15e84d
Yaroslav_Demin <demin@lanit.ru>
2023-12-30 18:53:28 +0300

    fix lint

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,10 @@ import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 import { ThemeProvider } from 'app/providers/ThemePropvider';
+import { StoreProvider } from 'app/providers/StoreProvider';
 import App from './app/App';
 import 'shared/config/i18n/i18n';
 import 'app/styles/index.scss';
-
-import { StoreProvider } from 'app/providers/StoreProvider';
 
 render(
     <StoreProvider>
