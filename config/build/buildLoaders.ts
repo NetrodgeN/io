43eb2c6bfb4dbcf9d314ece766facc30fea59379
Yaroslav_Demin <demin@lanit.ru>
2024-02-13 22:24:36 +0300

    add concurrently, new GARISH_BLUE theme, вынес babelLoader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,18 +1,12 @@
 import webpack from 'webpack';
 import { BuildOptions } from './types/config';
 import { buildCssLoaders } from './loaders/buildCssLoaders';
+import { buildBabelLoader } from './loaders/buildBabelLoader';
 
-export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
-    const babelLoader = {
-        test: /\.m?(js|jsx|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-            loader: 'babel-loader',
-            options: {
-                presets: ['@babel/preset-env'],
-            },
-        },
-    };
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+    const { isDev } = options;
+
+    const babelLoader = buildBabelLoader(options);
 
     const fileLoader = {
         test: /\.(png|jpe?g|gif|woff|woff2)$/i,
