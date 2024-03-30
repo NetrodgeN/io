755e6bc53296da83d9a1e2883d82b07e51eaa8a7
Yaroslav_Demin <demin@lanit.ru>
2024-03-30 11:27:01 +0300

    add user roles

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -3,7 +3,10 @@ export { getUserInited } from './model/selectors/getUserInited/getUserInited';
 export {
     User,
     UserSchema,
+    UserRole,
 } from './model/types/user';
+
+export { isUserAdmin, isUserManager, getUserRoles } from './model/selectors/roleSelectors';
 
 export {
     userReducer,
